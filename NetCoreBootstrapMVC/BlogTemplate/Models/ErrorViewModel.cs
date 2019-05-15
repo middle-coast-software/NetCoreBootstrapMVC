edae18c4df6a605f5482296368922591db970178
James Pusateri <james.pusateri@emptycoffee.com>
2019-05-15 11:46:51 -0500

    Base Projects Created
    No actual template work done yet

---
+++ NetCoreBootstrapMVC/BlogTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
